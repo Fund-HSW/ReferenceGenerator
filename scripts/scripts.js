50109f019a4b572abcaccddbb71e8164200c189f
Marcin Prokop <55412396+Prokop6@users.noreply.github.com>
2020-09-28 18:31:24 +0200

    Develop (#1)
    * dom mockup structure added
    
    * mockup references added
    
    * references creator working, references appending do site bottom
    
    * reference generation working, problem with swapping of elemets encountetd, working workaround implemented
    
    * href targets corrected

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+let separatorElement = document.createElement("hr");
+const refDiv = document.createElement("div");
+
+refDiv.id = "references";
+
+document.body.appendChild(separatorElement);
+document.body.append(refDiv);
+
+let refList = document.getElementsByTagName("ref");
+
+let i = 0;
+
+for (let item of refList) {
+    console.log(item.innerHTML)
+    const fnText = item.innerHTML
+    const refListId = i
+    const curId = ++i
+        
+    const refLink = document.createElement("a")
+    const refCont = document.createElement("sup")
+    refLink.id = `ref${curId}`
+    refLink.setAttribute("href",`#fn${curId}`)
+    refCont.innerText = `[${curId}]`
+    refLink.appendChild(refCont)
+
+    refList[refListId].innerText = ""
+    refList[refListId].appendChild(refLink)
+
+    const fnPara = document.createElement("p")
+    const fnParaLink = document.createElement("a")
+    const fnParaText = document.createElement("p")
+
+    fnParaLink.id = `fn${curId}`
+    fnParaLink.setAttribute("href",`#ref${curId}`)
+
+    fnParaLink.textContent = curId
+    fnParaText.textContent = fnText
+
+    fnPara.appendChild(fnParaLink)
+    fnPara.appendChild(fnParaText)
+
+    refDiv.appendChild(fnPara)
+};
